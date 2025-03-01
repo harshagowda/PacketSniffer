dd9bd3fd8e0c43f3da0108dc63299fd33f669d24
harshagowda <harsha.k.gowda@gmail.com>
2025-03-01 00:06:34 -0700

    Update main.cpp
    Adding first version and code formatting

--- main.cpp
+++ main.cpp
@@ -26,15 +26,15 @@ public:
 
 
 class PcapIf {
-	
-//	struct pcap_if {
-//	struct pcap_if *next;
-//	char *name;		/* name to hand to "pcap_open_live()" */
-//	char* description;	/* textual description of interface, or NULL */
-//	struct pcap_addr* addresses;
-//	bpf_u_int32 flags;	/* PCAP_IF_ interface flags */
-//	};
-	
+
+	//	struct pcap_if {
+	//	struct pcap_if *next;
+	//	char *name;		/* name to hand to "pcap_open_live()" */
+	//	char* description;	/* textual description of interface, or NULL */
+	//	struct pcap_addr* addresses;
+	//	bpf_u_int32 flags;	/* PCAP_IF_ interface flags */
+	//	};
+
 public:
 	string name;
 	string discription;
@@ -45,18 +45,18 @@ public:
 
 class LibPcap {
 public:
-	
+
 	char error_buffer[PCAP_ERRBUF_SIZE];
-	pcap_if_t* alldev_list_head ;
+	pcap_if_t* alldev_list_head;
 	vector<PcapIf> nw_devices;
 
 	LibPcap() {
 		pcap_if_t* alldevsp = nullptr;
 		int return_value = pcap_findalldevs(&alldevsp, error_buffer);
 
-		
+
 		if (return_value == PCAP_ERROR)
-			cerr << "Failed: pcap_findalldevs" <<endl;
+			cerr << "Failed: pcap_findalldevs" << endl;
 
 		alldev_list_head = alldevsp;
 
@@ -67,17 +67,18 @@ public:
 			nw_devices.emplace_back(temp);
 			alldevsp = alldevsp->next;
 		}
-		
+
 	}
 
 	template <typename K>
-	void copy_if_not_null(K *dest, K *src, unsigned long size)
+	void copy_if_not_null(K* dest, K* src, unsigned long size)
 	{
 		if (src)
 			memcpy(dest, src, size);
 		else
 			memset(dest, 0, size);
 	}
+
 	void copy_address(struct pcap_addr* addresses, PcapIf& PcapIftemp)
 	{
 
@@ -93,11 +94,11 @@ public:
 		}
 	}
 
-	void copy_device_data(pcap_if_t *pcap_device , PcapIf & PcapIftemp)
+	void copy_device_data(pcap_if_t* pcap_device, PcapIf& PcapIftemp)
 	{
 		PcapIftemp.name = string(pcap_device->name);
 
-		if(pcap_device->description!= NULL)
+		if (pcap_device->description != NULL)
 			PcapIftemp.discription = string(pcap_device->description);
 		PcapIftemp.flags = pcap_device->flags;
 		copy_address(pcap_device->addresses, PcapIftemp);
@@ -121,13 +122,10 @@ public:
 
 #if 1
 int main() {
+	LibPcap libpcap;
+	libpcap.print_devices();
 
-
-	{
-		LibPcap libpcap;
-		libpcap.print_devices();
-	}
-    return 0;
+	return 0;
 }
 
 #else
